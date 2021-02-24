74e231993282947bee3a4086e219018b0dd35a85
Timo <developer.timo@gmail.com>
2021-02-25 07:05:45 +0900

    HashMap 예제

--- src/ch15/map/HashMapExample1.java
+++ src/ch15/map/HashMapExample1.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-public class HashMapExample {
+public class HashMapExample1 {
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap<>();
 
